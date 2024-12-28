6a07ea8d7a8818cba72e7187ef26369b4b9407af
raghavt20 <raghavt20@aospa.co>
2024-12-28 20:13:47 +0530

    peridot: Update from V816.0.10.0.UNPMIXM
    Change-Id: If356cc20fc4c9251535b953f2cfe0e0b656fb575
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from peridot device
@@ -23,14 +23,17 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 24069PC21G
 
-PRODUCT_SYSTEM_NAME := peridot_global
-PRODUCT_SYSTEM_DEVICE := peridot
-
+# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.9.0.UNPMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.10.0.UNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys \
+    DeviceName=peridot \
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
 
-BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.9.0.UNPMIXM:user/release-keys
-
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# lineage Flags
+TARGET_BOOT_ANIMATION_RES := 1080
