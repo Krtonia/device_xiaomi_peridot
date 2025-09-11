c1cbedebebfaf259634346643902fcf62e401e17
Shikhar <Krtonia@gmail.com>
2025-09-11 22:49:31 +0530

    peridot: Bringup @The-Clover-Project
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,8 +107,8 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    swinfo.fingerprint=peridot:$(LINEAGE_VERSION) \
-    mtdoops.fingerprint=peridot:$(LINEAGE_VERSION)
+    swinfo.fingerprint=peridot:$(CLOVER_VERSION) \
+    mtdoops.fingerprint=peridot:$(CLOVER_VERSION)
 
 BOARD_KERNEL_CMDLINE += rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
@@ -151,7 +151,7 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(RAMDISK_MODULES_PATH)/mo
 CAMERA_PACKAGE_NAME := com.android.camera
 
 # Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/clover/config/BoardConfigReservedSize.mk
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
